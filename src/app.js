d8f4f444c5ecbc04d32b0d246eeec98e005ded6a
Anatoli33 <hadjiev1215@gmail.com>
2026-01-09 21:05:39 +0200

    Add mid screen

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import { showHome } from "./views/home.js";
 import { showAbout } from "./views/about.js";
 import { showContact } from "./views/contact.js";
 import { startEntryTest } from "./views/entryTest.js";
+import { showMid } from './views/mid.js';
 
 page("/", showHome);
 page("/about", showAbout);
 page("/contact", showContact);
 page("/test", startEntryTest);
+page("/mid", showMid);
 
 page.start({ click: true });
---
+++ src/views/mid.js
@@ -0,0 +1,21 @@
+import { render, html } from "lit-html";
+
+const main = document.getElementById("root");
+export function showMid() {
+    render(midTemp(), main);
+}
+
+function midTemp() {
+    return html`
+<section>
+  <div>
+    <h1>Избери предмет</h1>
+    <a href="/test" class="subject-button-link"><button>Български език - входно ниво</button></a>
+    <a href="/entryTestMath" class="subject-button-link"><button>Математика - входно ниво</button></a>
+    <a href="/entryTestEN" class="subject-button-link"><button>Английски език - входно ниво B1</button></a>
+    <a href="/bel" class="subject-button-link"><button>Български език</button></a>
+    <a href="/math" class="subject-button-link"><button>Математика</button></a>
+  </div>
+</section>
+    `;
+}
